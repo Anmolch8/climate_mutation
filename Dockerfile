0279195ce22217bdeece9efbc5e7e4533637f85b
Anmol chawla <chawllaa.anmol@gmail.com>
2023-01-11 22:00:35 +0530

    development server check

---
+++ Dockerfile
@@ -0,0 +1,26 @@
+FROM python
+LABEL maintainer='anmolch7'
+ENV PYTHONUNBUFFERED 1
+
+COPY ./climate_mutation /climate_mutation
+COPY ./requirements.txt /requirements.txt
+
+
+WORKDIR /climate_mutation
+
+EXPOSE 8000
+
+RUN python -m venv /py && \
+    /py/bin/pip install --upgrade pip && \
+    /py/bin/pip install -r /requirements.txt && \
+    adduser --disabled-password --no-create-home climate_user && \
+    mkdir -p /vol/web/static && \
+    mkdir -p /vol/web/media && \
+    chown -R climate_user:climate_user /vol && \
+    chmod -R 755 /vol
+
+ENV PATH="/py/bin:$PATH"
+
+USER climate_user
+
+
--- climate_mutation/climate_mutation/urls.py
+++ climate_mutation/climate_mutation/urls.py
@@ -18,7 +18,7 @@ from django.urls import path
 from climate_app import views
 from django.conf import settings
 from django.contrib.staticfiles.urls import static
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+#from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path("index/",views.index,name='index'),
@@ -59,7 +59,13 @@ urlpatterns = [
 
 ]
 
-urlpatterns+=staticfiles_urlpatterns()
-urlpatterns+=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+#urlpatterns+=staticfiles_urlpatterns()
+#urlpatterns+=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
 
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT,
+    )
 
